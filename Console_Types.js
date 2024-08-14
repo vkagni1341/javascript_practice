98f9d8de3ecfc498e1fcbdf9500d21db06215be1
Vipul Agnihotri <156226082+vkagni1341@users.noreply.github.com>
2024-08-15 00:47:12 +0530

    Console_Types

---
+++ Console_Types.js
@@ -0,0 +1,11 @@
+
+name = 'vipul';
+last = 'agnihotri';
+
+//TYPE-1
+
+console.log(`First name ${name.toUpperCase()} and last name is ${last.toUpperCase()}`);
+
+//TYPE -2
+
+console.table([name.toUpperCase(),last.toUpperCase()]);
