9873af76b9e9bf41c078818812d8e654b1ad2880
Vipul Agnihotri <156226082+vkagni1341@users.noreply.github.com>
2024-08-15 01:36:46 +0530

    Indian_Currency_Format

---
+++ Indian_Currency.js
@@ -0,0 +1,10 @@
+
+name = 'vipul'
+last = 'agnihotri'
+console.log(`First name ${name.toUpperCase()} and last name is ${last.toUpperCase()}`);
+
+
+
+value = 152200;
+//Represnt the Indian Currency 
+console.log(value.toLocaleString('en-in'));
