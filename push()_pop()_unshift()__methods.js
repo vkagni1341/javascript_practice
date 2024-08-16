cd6476f59cb1fc2b7d3e476b5d086feb1da38fad
Vipul Agnihotri <156226082+vkagni1341@users.noreply.github.com>
2024-08-16 17:46:15 +0530

    push()_pop()_unshift()__methods

---
+++ push()_pop()_unshift()__methods.js
@@ -0,0 +1,19 @@
+/*
+   push(),pop(),and unshift()
+*/
+const num = [1,2,5,8,9];
+
+
+console.log(`${num} is the Array number  `);
+console.log(typeof(num));
+
+num.push(8);
+console.table(num);
+
+
+num.pop();
+console.log(num);
+
+num.unshift(9);
+console.log(num);
+
