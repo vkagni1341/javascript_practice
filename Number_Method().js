ebf0c694f49c256175ae7021a9bef1a2ade19ec9
Vipul Agnihotri <156226082+vkagni1341@users.noreply.github.com>
2024-08-16 17:46:45 +0530

    Number_Method()

---
+++ Number_Method().js
@@ -0,0 +1,18 @@
+/*
+  Number Related method() :-
+  ==========================         */
+
+num1 = -4.6;
+num2 = 5.2;
+
+console.log(Math.abs(num1));
+console.log(Math.abs(num2));
+
+console.log(Math.round(num1));
+console.log(Math.round(num2));
+
+console.log(Math.ceil(num1));
+console.log(Math.ceil(num2));
+
+console.log(Math.floor(num1));
+console.log(Math.floor(num2));
