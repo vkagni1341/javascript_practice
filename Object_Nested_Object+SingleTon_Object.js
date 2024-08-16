8e8e4da5ef8c3baa82d1f0b8d0bd78e8a8ac0670
Vipul Agnihotri <156226082+vkagni1341@users.noreply.github.com>
2024-08-17 00:29:29 +0530

    Object_Nested_Object+SingleTon_Object

---
+++ Object_Nested_Object+SingleTon_Object.js
@@ -0,0 +1,19 @@
+ /*
+   Nested Object 
+   
+ */
+ const obj = new Object() //this is SingleTon Object
+ console.log(`${typeof obj}, It is a SingleTon Object`);
+ 
+ 
+ const obj1 = 
+ {
+   name : 
+   {
+     fname : "vipul" ,
+     lname : "Agnihotri"
+   },
+   age : 19,
+   city : "HYD"
+ }
+ console.log(obj1);
