1af2e1a2be83e7f5635ed91c3d3ba88ea81b8811
Vipul Agnihotri <156226082+vkagni1341@users.noreply.github.com>
2024-08-16 17:47:21 +0530

    Number_Find_Unique_Number_Random()

---
+++ Number_Find_Unique_Number_Random().js
@@ -0,0 +1,10 @@
+/*
+   Number Find unique value[Math.random()] :-
+   ==========================================              */
+
+min = 10;
+max = 20;
+
+//Find Between range Unique Value 
+console.log(Math.floor(Math.random()*(max-min + 1)+min));
+console.log(Math.floor(Math.random()*(max-min + 1)+max));
