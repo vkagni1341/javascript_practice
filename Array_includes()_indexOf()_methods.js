680394c039332f31f64c83baf700e4f226c5d495
Vipul Agnihotri <156226082+vkagni1341@users.noreply.github.com>
2024-08-18 18:51:33 +0530

    Array_includes()_indexOf()_methods

---
+++ Array_includes()_indexOf()_methods.js
@@ -0,0 +1,15 @@
+/*
+  includes(),indexOf()
+*/
+const num = [1,2,5,8,9];
+
+
+console.log(`${num} is the Array number  `);
+console.log(typeof(num));
+
+//It return Boolean value
+console.log(num.includes(10));
+
+//index number is aviable it given 1 otherwise -1 given
+console.log(num.indexOf(10));
+
