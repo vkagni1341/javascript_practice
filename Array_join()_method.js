9ed1f48af6370b3323d8ed87ebf29f56c2a40a87
Vipul Agnihotri <156226082+vkagni1341@users.noreply.github.com>
2024-08-18 18:51:56 +0530

    Array_join()_method

---
+++ Array_join()_method.js
@@ -0,0 +1,16 @@
+/*
+  join()
+*/
+const num = [1,2,5,8,9];
+
+
+console.log(`${num} is the Array number  `);
+console.log(typeof(num));
+
+//if we are using the join() the array data are change into the String format
+const mum =  num.join();
+console.log(`${mum} this is a  ${typeof(mum)} .`);
+console.log(typeof(mum));
+
+
+
